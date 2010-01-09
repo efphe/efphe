606b035e130ffef2d1d728d8a03b752d92f07572
efphe <effetom@gmail.com>
2010-01-09 17:43:55 +0100

    lnkshrt.py

---
+++ lnkshrt.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# License: do with this code what you want
+# Author: Federico Tomassini, aka efphe
+#   Thanks to: http://wubook.net/, http://en.wubook.net/
+
+import urllib as _
+import sys as __
+
+def _g(u):
+  e= _.urlencode({'url': u})
+  f= _.urlopen('http://dr.tl/?module=ShortURL&file=Add&mode=API', e)
+  return f.read()
+
+if __name__ == '__main__':
+  try:
+    url= __.argv[1]
+    f= _g(url)
+    print f
+  except:
+    print \
+"""Usage:
+  lnkshrt.py url
+
+You must specify `url`"""
